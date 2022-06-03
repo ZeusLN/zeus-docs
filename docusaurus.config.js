b86e04841102d3e7345a91fb3d64187e5f38b1ec
Evan Kaloudis <kaloudis@users.noreply.github.com>
2022-06-03 16:07:25 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ZeusLN/zeus-docs/tree/main/docs/',
+            'https://github.com/ZeusLN/zeus-docs/tree/main/',
         },
         blog: false,
         theme: {
